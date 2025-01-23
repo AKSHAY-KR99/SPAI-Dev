261939e1c66cce525bbd286fa3a96678a3de34da
AKSHAY-KR99 <akshayakr4@gmail.com>
2025-01-23 23:33:05 +0530

    User ingestion API added, Cerificate signature changes

--- spai_dev/spai_app/serializers.py
+++ spai_dev/spai_app/serializers.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from rest_framework import serializers
-from .models import LifeMembers
+from datetime import datetime, timedelta
+from .utils import get_registration_num
+from .models import LifeMembers, User, UserDetailModel, PaymentModel, AnnualSubscriptionModel
 
 
 class LifeMembersSerializer(serializers.ModelSerializer):
@@ -23,3 +26,40 @@ class LifeMembersSerializer(serializers.ModelSerializer):
         fields = ['reg_no', 'name', 'address', 'mobile', 'email', 'membership_date', 'upload_date', 'update_date',
                   'active', 'uid', 'lm_key']
         read_only_fields = ['upload_date', 'update_date']
+
+
+# User direct ingestion API
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'first_name', 'last_name', 'state', 'annual_subscription',
+                  'status', 'user_role', 'reg_no', 'admin_approved', 'date_approved', 'approval_percentage',
+                'active_key']
+    def create(self, validated_data):
+        validated_data['status'] = settings.ADMIN_APPROVED
+        validated_data['user_role'] = settings.MEMBER_ROLE_VALUE
+        validated_data['admin_approved'] = True
+        validated_data['date_approved'] = datetime.now()
+        validated_data['approval_percentage'] = 100
+        validated_data['active_key'] = True
+        validated_data['reg_no'] = get_registration_num()
+        return super().create(validated_data)
+
+class UserDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserDetailModel
+        fields = ['user', 'degree', 'profession', 'institution', 'department', 'address', 'phone_number', 'alternate_number',
+                  'alternate_mail', 'specialized_in', 'research_interest']
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PaymentModel
+        fields = ['user_info', 'transaction_id', 'reference_id', 'bank_name', 'payment_type', 'document']
+
+
+class AnnualSubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AnnualSubscriptionModel
+        fields = ['user', 'date_created', 'end_date', 'active']
